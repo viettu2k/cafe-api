437b1564b5235082181b5fdeec5b8582276b56da
viettu2k <doanviettu2k@gmail.com>
2022-07-29 09:51:51 +0700

    adding get PDF route

--- controllers/bill.js
+++ controllers/bill.js
@@ -65,4 +65,46 @@ const generateReport = (req, res) => {
   );
 };
 
-module.exports = { generateReport };
+const getPDF = (req, res) => {
+  const { name, email, contact, paymentMethod, total, productDetails, uuid } =
+    req.body;
+  const pdfPath = `./generated_pdf/${uuid}.pdf`;
+  if (fs.existsSync(pdfPath)) {
+    res.contentType("application/pdf");
+    fs.createReadStream(pdfPath).pipe(res);
+  } else {
+    const productDetailsReport = JSON.parse(productDetails);
+    ejs.renderFile(
+      path.join(__dirname, "", "report.ejs"),
+      {
+        productDetails: productDetailsReport,
+        name,
+        email,
+        contact,
+        paymentMethod,
+        total,
+      },
+      (error, data) => {
+        if (!error) {
+          pdf
+            .create(data)
+            .toFile(`./generated_pdf/${uuid}.pdf`, (err, data) => {
+              if (err) {
+                return res.status(500).json(error_);
+              } else {
+                res.contentType("application/pdf");
+                fs.createReadStream(pdfPath).pipe(res);
+                return res.status(200).json({
+                  uuid: generateUuid,
+                });
+              }
+            });
+        } else {
+          return res.status(500).json(error);
+        }
+      }
+    );
+  }
+};
+
+module.exports = { generateReport, getPDF };
--- routes/bill.js
+++ routes/bill.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { authenticateToken } = require("../services/auth");
 const { checkRole } = require("../services/checkRole");
-const { generateReport } = require("../controllers/bill");
+const { generateReport, getPDF } = require("../controllers/bill");
 
 router.post("/generateReport", authenticateToken, checkRole, generateReport);
+
+router.post("/getPDF", authenticateToken, checkRole, getPDF);
 
 // router.patch("/update", authenticateToken, checkRole, updateCategory);
 
